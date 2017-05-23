dd52e502c1e060bc0c69d5e7970b6ad257967ec5
Shiqi Zhang <shiqizhang@Shiqis-MacBook-Pro-3.local>
2017-05-23 16:36:10 -0400

    editing from Shiqi Zhang

--- MyLinuxKernel/src/main/java/LinuxKernel.java
+++ MyLinuxKernel/src/main/java/LinuxKernel.java
@@ -2,27 +2,34 @@ package org.reroutlab.code.auav.kernels;
 
 import org.reroutlab.code.auav.drivers.ExternalCommandsDriver;
 import org.reroutlab.code.auav.drivers.LocationManagerDriver;
+import org.reroutlab.code.auav.drivers.CatchImageDriver;
 import java.io.IOException;
 import java.util.*;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
-
+/**
+ *
+ *This class works like handler, connects all drivers
+ *
+ *
+ */
 public class LinuxKernel {
-		private Level AUAVLEVEL = Level.FINE;
+		private Level AUAVLEVEL = Level.FINE; // set AUAVLEVEL
 		private static Logger theLogger =
-				Logger.getLogger(LinuxKernel.class.getName());
+				Logger.getLogger(LinuxKernel.class.getName());//get Logger object by calling getLogger receive the name of the LinuxKernal.class'name
 
 
-		HashMap n2p = new HashMap<String, String>();
+		HashMap n2p = new HashMap<String, String>(); 
 
-		public LinuxKernel ()  {
-				theLogger.setLevel(AUAVLEVEL);
+		public LinuxKernel ()  {//setup constructor 
+				theLogger.setLevel(AUAVLEVEL); //set logger's level
 
 				// Create new driver objects
 				theLogger.log(Level.FINE,"Creating driver objects");
 				ExternalCommandsDriver ecd = new ExternalCommandsDriver();
 				LocationManagerDriver lm = new LocationManagerDriver();
+				CatchImageDriver cid = new CatchImageDriver();
 				
 				// Gather name to ports/usage mapping
 				theLogger.log(Level.FINE,"Creating driver-to-port mapping");				
@@ -30,13 +37,19 @@ public class LinuxKernel {
 								new String("Port:"+ecd.getLocalPort()+"\n" ) );
 				n2p.put(lm.getClass().getCanonicalName(),
 								new String("Port:"+lm.getLocalPort()+"\n" ) );
+				n2p.put(cid.getClass().getCanonicalName(),
+								new String("Port:"+cid.getLocalPort()+"\n" ) );
+
 				
 
 				// Send the map back to each object
 				ecd.setDriverMap(n2p);
-				lm.setDriverMap(n2p);				
+				lm.setDriverMap(n2p);
+				cid.setDriverMap(n2p);				
 				ecd.setLogLevel(AUAVLEVEL);
 				lm.setLogLevel(AUAVLEVEL);
+				cid.setLogLevel(AUAVLEVEL);
+
 
 				// Printing the map object locally for logging
 				String mapAsString = "Active Drivers\n";
@@ -51,9 +64,12 @@ public class LinuxKernel {
 				// Start each thread
 				theLogger.log(Level.FINE,"Activating threads");				
 				Thread ecdT = new Thread(ecd);
-				ecdT.start();
+				ecdT.start();//This will call run method automatically
 				Thread lmT = new Thread(lm);
-				lmT.start();				
+				lmT.start();
+				Thread cidT = new Thread(cid);
+				cidT.start();
+				
 				
 		}
 
@@ -61,3 +77,4 @@ public class LinuxKernel {
 				LinuxKernel k = new LinuxKernel();
 		}
 }
+
--- src/org/reroutlab/code/auav/drivers/ExternalCommandsDriver/src/main/java/ExternalCommandsDriver.java
+++ src/org/reroutlab/code/auav/drivers/ExternalCommandsDriver/src/main/java/ExternalCommandsDriver.java
@@ -24,16 +24,31 @@ import java.util.logging.Level;
  * @since   2017-05-01 
  */
 
-public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.drivers.AuavDrivers {
+public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.drivers.AuavDrivers { //implement allows to use Thread
+		//indicate port number
 		private int ECD_PORT = 5117;
 
     private static Logger ecdLogger =
-				Logger.getLogger(ExternalCommandsDriver.class.getName());
+				Logger.getLogger(ExternalCommandsDriver.class.getName());//return the name of the entity represented by this class object
+    				//get Logger object by calling getLogger receive the name of the ExternalCommandDriver.class'name
+		/**
+		 *
+		 *
+		 * This specifying which message levels will be logged by this logger
+		 *
+		 */
 		public void setLogLevel(Level l) {
 				ecdLogger.setLevel(l);
-		}		
+						}		
 		
-		private ServerSocket serverSocket;
+		private ServerSocket serverSocket; //declare a SeverSocket
+		/**
+		 *
+		 * This function just to check if serverSocket is null, set the local port number in to -1
+		 * 
+		 * @return local port number
+		 *
+		 */
 		public int getLocalPort() {
 				if (serverSocket == null) {
 						return -1;
@@ -42,6 +57,15 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 		}
 		
 		private String usageInfo="CMD param=value param=value param=value; list ";
+
+		/**
+		 *
+		 * This function return the design string to respond to call
+		 *
+		 *
+		 * @return usageInfo 
+		 *
+		 */
 		public String getUsageInfo() {
 				return usageInfo;
 		}
@@ -49,6 +73,14 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 		
 
 		private HashMap driver2port;  // key=drivername value={port,usageInfo}
+
+		/**
+		 *
+		 * This function help construct Map
+		 *
+		 * @param HashMap<String, String> m
+		 *
+		 */
 		public void setDriverMap(HashMap<String, String> m) {
 				if (m != null) {
 						driver2port = new HashMap<String, String>(m);
@@ -66,30 +98,37 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 				
 		}
 
+		/**
+		 * This function keep reading string from user
+		 *
+		 */
+
 		public void run() {
 				BufferedReader bufferedReader;
 				PrintWriter printWriter;
+				//decaler BufferedReader and PrintWriter
 				while (true) {
 						Socket clientSocket=null;
 						try {
-								clientSocket = serverSocket.accept();
+								clientSocket = serverSocket.accept(); //listen for the connection made for this socket and accept it
 								bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 								printWriter = new PrintWriter(clientSocket.getOutputStream(),true);
 
-								String inputLine = bufferedReader.readLine();
-								String lastLine = "";
+								String inputLine = bufferedReader.readLine(); // read input by line
+								String lastLine = ""; 
 								if (inputLine.equals("mult")) {
+									// if the first line is equal to mult get into loop until met another mult
 										do {
 												lastLine = bufferedReader.readLine();
 												inputLine = inputLine + "\n" + lastLine;
 										} while (lastLine.equals("mult") == false);
 								}
 								if (inputLine != null) {
-										inputLine = inputLine.trim();
+										inputLine = inputLine.trim(); //return without leading and trailing white spaces
 										String outputLine = processCommands(inputLine);
 										printWriter.println(outputLine);
 								}
-								clientSocket.close();
+								clientSocket.close(); //close socket
 						}
 						catch (Exception e) {
 								ecdLogger.log(Level.WARNING, "Problem: " + e.toString() );
@@ -103,6 +142,15 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 				
 		}
 
+		/**
+		 *
+		 * This function process the input commands, if the there is list, append to the string, if not call sendTo methond find out the matching information
+		 *
+		 * @return information in the list
+		 *
+		 *
+		 */
+
 
 		private String processCommands(String inputLine) {
 				// Split on \n then on ' '
@@ -112,7 +160,7 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 						String[] args = cmd.split(" ");
 						// Format: driver_name driver_cmd [driver_prm=driver_arg]*
 						if (args[0].equals("list")) {
-								Set keys = driver2port.keySet();
+								Set keys = driver2port.keySet(); //HashMap<port, usageInfo>
 								String output = "";
 								for (Iterator i = keys.iterator(); i.hasNext(); ) {
 										String name = (String) i.next();
@@ -137,10 +185,18 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 				return outLine;
 		}
 
+		/**
+		 *
+		 * This information grab the port number in the command line and return the according input information from matching socket
+		 *
+		 * @return the information from specified port number
+		 *
+		 */
+
 		private String sendTo(String p, String c) {
 				String[] portInfo = p.split(":");
 				try {
-						Socket echoSocket = new Socket("127.0.0.1", Integer.parseInt(portInfo[1].trim()) );
+						Socket echoSocket = new Socket("127.0.0.1", Integer.parseInt(portInfo[1].trim()) ); // create the new socket
 						PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
 						BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
 						out.println(c);
@@ -154,4 +210,5 @@ public class ExternalCommandsDriver implements Runnable,org.reroutlab.code.auav.
 				return("Error in sendTo prevented communication");
 		}
 }
+
 
