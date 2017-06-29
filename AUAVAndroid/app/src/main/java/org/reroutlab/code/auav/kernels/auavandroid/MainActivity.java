e583cca6e098c2dafc03cba362a9a1a0752af7f9
Christopher Stewart <cstewart@localhost.localdomain>
2017-06-29 13:56:18 -0400

    CS: Fixed interface --> interfaces.jar

--- AUAVAndroid/app/src/main/java/org/reroutlab/code/auav/kernels/auavandroid/MainActivity.java
+++ AUAVAndroid/app/src/main/java/org/reroutlab/code/auav/kernels/auavandroid/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DJICamera camera = App.getCameraInstance();
+
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
