0355696637008771315df9de2445dfe0e3576da0
Christopher Stewart <cstewart@localhost.localdomain>
2017-06-29 16:00:45 -0400

    CS: new routines interface

---
+++ interfaces/src/main/java/AuavRoutines.java
@@ -0,0 +1,11 @@
+package org.reroutlab.code.auav.AuavRoutines;
+
+
+import java.util.HashMap;
+
+public interface AuavRoutines {
+		
+		public void startRoutine();
+		public void stopRoutine();		
+		public void getDriverMap(HashMap<String,String> m);
+}
