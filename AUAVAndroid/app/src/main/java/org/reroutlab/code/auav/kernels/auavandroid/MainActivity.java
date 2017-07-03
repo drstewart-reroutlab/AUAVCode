fb336231bad5c5198d54e4c595d56af21e34dcdb
Christopher Stewart <cstewart@localhost.localdomain>
2017-07-03 14:25:10 -0400

    CS: MainActivity.java

--- AUAVAndroid/app/src/main/java/org/reroutlab/code/auav/kernels/auavandroid/MainActivity.java
+++ AUAVAndroid/app/src/main/java/org/reroutlab/code/auav/kernels/auavandroid/MainActivity.java
@@ -53,6 +53,16 @@ public class MainActivity extends AppCompatActivity {
                 new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.INTERNET}, 0);
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.VIBRATE,
+                        Manifest.permission.ACCESS_WIFI_STATE,
+                        Manifest.permission.WAKE_LOCK, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_NETWORK_STATE, Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.CHANGE_WIFI_STATE, Manifest.permission.MOUNT_UNMOUNT_FILESYSTEMS,
+                        Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.SYSTEM_ALERT_WINDOW,
+                        Manifest.permission.READ_PHONE_STATE,
+                }
+                , 0);
 
         Thread t = new Thread() {
             public void run() {
